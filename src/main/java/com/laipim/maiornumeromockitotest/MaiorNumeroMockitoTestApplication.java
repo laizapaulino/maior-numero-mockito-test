d36bf6f4b10b9b9708c19f513e662635e2fe4d2c
laizapaulino <laizapaulino1@gmail.com>
2021-05-02 20:17:48 -0300

    primeiro commit

---
+++ src/main/java/com/laipim/maiornumeromockitotest/MaiorNumeroMockitoTestApplication.java
@@ -0,0 +1,13 @@
+package com.laipim.maiornumeromockitotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiorNumeroMockitoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiorNumeroMockitoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laipim/maiornumeromockitotest/MaiorNumeroMockitoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laipim.maiornumeromockitotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiorNumeroMockitoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
